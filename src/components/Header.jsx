8aedcaef9185ad385738bf5b0186b40833dbc2ca
ArnoldMartinez <arnold.vilela.98@gmail.com>
2023-08-06 17:39:18 -0500

    Buscador de Peliculas terminado v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Nav from './Nav'
 const Header = () => {
   return (
     <header className='relative grid gap-2 py-4 text-white place-items-center'>
-      <h1 className='text-3xl font-bold'>Buscador de Peliculas</h1>
+      <h1 className='text-3xl font-bold'>Buscador de Películas</h1>
       <Nav />
     </header>
   )
